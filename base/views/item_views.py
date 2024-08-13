8dd7a16cd8fae088b40c1d7e2d18e18edea40d47
user <user@usernoMacBook-Air.local>
2024-08-13 10:42:56 +0900

    list__view

--- base/views/item_views.py
+++ base/views/item_views.py
@@ -5,4 +5,4 @@ from base.models import Item
 class ItemListView(generic.ListView):
     model=Item
     template_name='pages/item_list.html'
-    
+    queryset=Item.objects.filter(is_published=True)    
